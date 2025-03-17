bf8b2db1c3555629c606c9c203736e7238220cac
Anton Badretdinov <antonbad03@gmail.com>
2025-03-17 22:34:07 +0300

    + clear main

--- main.py
+++ main.py
@@ -1,15 +1,2 @@
-from entities.pump import Pump
 
-# насос
-pump_example = Pump(pump_id="P001")
-print(pump_example)
-pump_example.turn_on()
-pump_example.set_power(15)
-print(f"Текущая мощность насоса: {pump_example.get_power()} кВт")
-pump_example.set_speed(1500)
-pump_example.set_pressure(10)
-pump_example.update_temperature(30)
-print(pump_example)
-pump_example.turn_off()
-pump_example.set_power(10)
-print(pump_example)
+# Empty logic
